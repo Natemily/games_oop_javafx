0d2f779f9cb6b82e8d614aa74b828d4630940ed5
Natemily <nerezko.n@gmail.com>
2022-03-03 10:51:44 +0300

    2. Каркас шахматной доски [#792 #270687]

--- chess/src/test/java/ru/job4j/chess/firuges/black/BishopBlackTest.java
+++ chess/src/test/java/ru/job4j/chess/firuges/black/BishopBlackTest.java
@@ -10,30 +10,35 @@ import org.junit.Test;
 import static org.hamcrest.Matchers.is;
 import static org.junit.Assert.assertThat;
 
-public class BishopBlackTest extends TestCase {
+public class BishopBlackTest {
 
+    @Test
     public void testPosition() {
         BishopBlack bishopBlack = new BishopBlack(Cell.C1);
         Cell expected = bishopBlack.position();
         assertThat(expected, is(Cell.C1));
     }
 
-   public void testWayRight() {
+    @Test
+    public void testWayRight() {
         BishopBlack position = new BishopBlack(Cell.D3);
         Cell[] cell = new Cell[] {Cell.C2, Cell.B1};
         Cell[] expected = position.way(Cell.B1);
         assertThat(expected, is(cell));
     }
 
-    /*public void testWayNotRight() throws ImpossibleMoveException {
+    @Test(expected = ImpossibleMoveException.class)
+    public void testWayNotRight() {
         BishopBlack source = new BishopBlack(Cell.D3);
         Cell[] cell = source.way(Cell.B2);
-    }*/
+    }
 
+    @Test
     public void testCopy() {
         BishopBlack bishopBlack = new BishopBlack(Cell.G5);
         bishopBlack.copy(Cell.G5);
         Cell expected = bishopBlack.position();
         assertThat(expected, is(Cell.G5));
     }
+
 }
